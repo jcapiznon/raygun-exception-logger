e932b146b054220165674bff09ac62416709f3fe
jcapiznon <jcapiznon@reekoh.com>
2017-02-24 11:33:29 +0800

    add logs

--- app.js
+++ app.js
@@ -6,17 +6,19 @@ const _plugin = new reekoh.plugins.ExceptionLogger()
 let raygunClient =  null
 
 _plugin.on('exception', (error) => {
-  console.log('-exception- ', error )
   raygunClient.send(error, {}, (response) => {
     if (response.statusCode === 202) return
 
     console.error('Error on Raygun.', response.statusMessage)
     _plugin.logException(new Error(response.statusMessage))
   })
+  _plugin.log(JSON.stringify({
+    title: 'Exception sent to Raygun',
+    data: {message: error.message, stack: error.stack, name: error.name}
+  }))
 })
 
 _plugin.once('ready', () => {
-  console.log('-ready-')
   let raygun = require('raygun')
   raygunClient = new raygun.Client().init({ apiKey: _plugin.config.apiKey})
 
